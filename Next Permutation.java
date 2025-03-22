c1b43443c2bf0fcbdeb6dbecce56f7760cb6a0dd
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-03-22 11:27:31 +0530

    Update Next Permutation.java

--- Next Permutation.java
+++ Next Permutation.java
@@ -1,40 +1,48 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        //find pivot
-        int pivot = -1;
-        for(int i = nums.length-2; i >= 0; i--){
-            if(nums[i] < nums[i+1]){
-                pivot = i;
+    void nextPermutation(int[] arr) {
+        // code here
+        int n = arr.length;
+    
+        // task:- 1  find the blue gola.........
+        int gola_index = -1;
+            
+        for(int i = n-1; i > 0; i--){
+            if(arr[i] > arr[i - 1]){
+                gola_index = i-1;
                 break;
             }
         }
-
-        if(pivot == -1){
-            Arrays.sort(nums);  // in place changes
-            return;            
+        
+        //if no such index found, reverse the array (last permutation)
+        
+        if(gola_index == -1){
+            Arrays.sort(arr);
+            return;
         }
-
-        // to find right most elemnt > pivot and swap (RME, Piovt)
-        for(int i = nums.length-1; i > pivot; i--){
-            if(nums[i] > nums[pivot]){
-                int temp = nums[i];
-                nums[i] = nums[pivot];
-                nums[pivot] = temp;
+        
+        //task:- 2  find next greater element form gola_index
+        
+        int swap_index = gola_index;
+        
+        for(int j = n-1; j > gola_index; j--){
+            if(arr[j] > arr[gola_index]){
+                swap_index = j;
                 break;
             }
-
         }
-
-        //reverse (pivot + 1) to nums.length - 1
-
-        int i = pivot + 1 ;
-        int j = nums.length - 1;
-        while(i <= j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+        
+        //Task:- 3  swap
+        //Arrays.swap(arr[gola_index], arr[swap_index])
+        {
+        int temp = arr[gola_index];
+        arr[gola_index] = arr[swap_index];
+        arr[swap_index] = temp;
         }
+        
+        //Task :- 4  sort array
+        //Arryas.sort(arr_name , starting_index , last_index)
+        Arrays.sort(arr, gola_index + 1, n);
+        
+        
     }
 }
