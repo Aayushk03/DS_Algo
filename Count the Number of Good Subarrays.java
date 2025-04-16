72d2741076762466da11598b365b5533dfe4279b
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-04-16 19:10:22 +0530

    Create Count the Number of Good Subarrays.java

---
+++ Count the Number of Good Subarrays.java
@@ -0,0 +1,32 @@
+//Leetcode Link   : https://leetcode.com/problems/count-the-number-of-good-subarrays?envType=daily-question&envId=2025-04-16
+//Approach (Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
+
+class Solution {
+    public long countGood(int[] nums, int k) {
+        int n = nums.length;
+        int i = 0, j = 0;
+
+        long result = 0;
+        long pairs = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while (j < n) {
+            pairs += map.getOrDefault(nums[j], 0);
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            while (pairs >= k) {
+                result += (n - j);
+                map.put(nums[i], map.get(nums[i]) - 1);
+                pairs -= map.get(nums[i]);
+                i++;
+            }
+
+            j++;
+        }
+
+        return result;
+    }
+}
