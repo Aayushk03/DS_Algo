c9fd30d37a26f3cb14de0e59c941b4a85e120b0f
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-05-17 09:46:51 +0530

    Create Sort Colors.java

---
+++ Sort Colors.java
@@ -0,0 +1,34 @@
+/*
+  Approach-(Follow up) : O(n)
+  T.C : O(n)
+  S.C : O(1)
+  Leetcode Link : https://leetcode.com/problems/sort-colors/
+*/
+ 
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+
+        int i = 0;   // denotes for 0
+        int j = 0;   // denotes for 1
+        int k = n - 1; // denotes for 2
+
+        while (j <= k) {
+            if (nums[j] == 1) {
+                j++;
+            } else if (nums[j] == 2) {
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                k--;
+            } else { // nums[j] == 0
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
