60b7e7e515c5220273cca5a63d70c6ff6f7271c1
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-06-10 11:27:43 +0530

    Create Maximum Difference Between Even and Odd Frequency I.java

---
+++ Maximum Difference Between Even and Odd Frequency I.java
@@ -0,0 +1,33 @@
+/*
+Approach (Straught forward maxOdd - minEven)
+T.C : O(n)
+S.C : O(1)
+Leetcode Link : https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/description/
+*/
+
+class Solution {
+    public int maxDifference(String s) {
+        int n = s.length();
+
+        int maxOdd = 0;
+        int minEven = n + 1;
+
+        int[] freq = new int[26]; // for 'a' to 'z'
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+
+            if (freq[i] % 2 == 0) {
+                minEven = Math.min(minEven, freq[i]);
+            } else {
+                maxOdd = Math.max(maxOdd, freq[i]);
+            }
+        }
+
+        return maxOdd - minEven;
+    }
+}
