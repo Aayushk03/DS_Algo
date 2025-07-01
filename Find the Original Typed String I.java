cb30395c4c5135df6b20f6493fce1f1f3d0223df
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-07-01 09:20:41 +0530

    Create Find the Original Typed String I.java

---
+++ Find the Original Typed String I.java
@@ -0,0 +1,20 @@
+/*
+Leetcode Link  : https://leetcode.com/problems/find-the-original-typed-string-i
+Approach (straight forward traversal)
+T.C : O(n)
+S.C : O(1)
+*/
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int count = 0;
+
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        return count + 1; // +1 for the base case (no long presses)
+    }
+}
