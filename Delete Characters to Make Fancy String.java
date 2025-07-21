bdc933ee9f99e7bf37ad72c34c6d842b5c4dccc0
Aayush Kumar Shaw <121482765+Aayushk03@users.noreply.github.com>
2025-07-21 12:32:16 +0530

    Create Delete Characters to Make Fancy String.java

---
+++ Delete Characters to Make Fancy String.java
@@ -0,0 +1,31 @@
+/*
+Leetcode Link : https://leetcode.com/problems/delete-characters-to-make-fancy-string
+Approach (Simple straight forward traversal)
+T.C : O(n)
+S.C : O(1)
+*/
+
+class Solution {
+    public String makeFancyString(String s) {
+        int n = s.length();
+        StringBuilder result = new StringBuilder();
+
+        // Start with the first character
+        result.append(s.charAt(0));
+        int freq = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == result.charAt(result.length() - 1)) {
+                freq++;
+                if (freq < 3) {
+                    result.append(s.charAt(i));
+                }
+            } else {
+                result.append(s.charAt(i));
+                freq = 1;
+            }
+        }
+
+        return result.toString();
+    }
+}
